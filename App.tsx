51b9f473ec8fb09f9b03f7a3a997b532bcc4a17b
sumanta98 <sumanta.paikaray@applore.in>
2024-02-11 15:00:29 +0530

    Amatic Font added

--- App.tsx
+++ App.tsx
@@ -1,9 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import DayListItem from "./src/components/core/DayListItem";
+import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
+import {
+  AmaticSC_400Regular,
+  AmaticSC_700Bold,
+} from "@expo-google-fonts/amatic-sc";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+SplashScreen.preventAutoHideAsync();
 
 const days = [...Array(24)].map((val, index) => index + 1);
 export default function App() {
+  let [fontsLoaded, fontError] = useFonts({
+    Inter: Inter_900Black,
+    Amatic: AmaticSC_400Regular,
+    AmaticBold: AmaticSC_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
     <View style={styles.container}>
       <FlatList
@@ -23,8 +45,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
     gap: 10,
   },
   content: {
